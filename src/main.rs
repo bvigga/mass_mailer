b4922bc2b1870e5192b2be17b2fee2871c022224
Bernard Vigga <bvigga@gmail.com>
2024-04-26 22:14:52 +1200

    health_check api implemented with limited tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-fn main() {
-    println!("Hello, world");
+use std::net::TcpListener;
+
+use mass_mailer::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
 
 
