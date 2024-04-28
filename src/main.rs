8a43d7f96208731a2a1022693459d6edd80eaec1
Bernard Vigga <bvigga@gmail.com>
2024-04-28 14:12:55 +1200

    Chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-
-use mass_mailer::run;
+use mass_mailer::configuration::get_configuration;
+use mass_mailer::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Bubble up the io::Error if we failed to bind the address
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = TcpListener::bind(address)?;
+    run(address, connection_pool)?.await?;
+    Ok(())
 }
 
 
